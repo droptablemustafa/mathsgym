00611daf5e800fcb7fb511cc2b02c0270583c8d9
droptablemustafa <120393372+droptablemustafa@users.noreply.github.com>
2025-01-24 21:25:11 +0000

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./index.css";
 import Home from "./Home.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
+		<SpeedInsights/>
 		<Router>
 			<Routes>
 				<Route
